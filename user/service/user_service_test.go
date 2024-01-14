2599c689e26f383fedb8e590133cb2af8a77bdf8
adzi fauzi ahmad <adzi.fauziahmad@gmail.com>
2024-01-15 01:47:20 +0700

    try to create test in user service

---
+++ user/service/user_service_test.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"adzi-clean-architecture/domain"
+	"adzi-clean-architecture/domain/mocks"
+	"fmt"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func TestGetUserAll(t *testing.T) {
+	mockUserRepo := new(mocks.UserRepository)
+
+	id1, _ := primitive.ObjectIDFromHex("62cda02bf5a4d0c8cbf3795a")
+
+	mockUsers := domain.User{
+		Id:           id1,
+		Nama:         "Udin",
+		Username:     "udin2023",
+		Password:     "pas123",
+		Email:        "udinxxxx2023@gmail.com",
+		Phone:        "09899887",
+		Token:        "",
+		UserType:     "",
+		RefreshToken: "",
+	}
+
+	mockListUser := make([]*domain.User, 0)
+	mockListUser = append(mockListUser, &mockUsers)
+
+	// mockUserRepo.On("GetUserAll")
+
+	mockUserRepo.On("GetUserAll").Return(mockListUser)
+
+	// Arrange
+	realService := NewUserService(mockUserRepo)
+	// mockService := new(mocks.UserService)
+
+	resultMocked := realService.GetUserAll()
+
+	fmt.Println(resultMocked[0].Nama)
+
+}
